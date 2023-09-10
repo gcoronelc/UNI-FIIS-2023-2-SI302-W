6452288628303fd122c1a72ae5ef937bcb4c4428
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2023-09-10 09:13:30 -0500

    Semana 2

---
+++ Semana02/Semana02/src/pe/edu/uni/semana02/prueba/Prueba02.java
@@ -0,0 +1,16 @@
+package pe.edu.uni.semana02.prueba;
+
+import pe.edu.uni.semana02.uno.ClaseB;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class Prueba02 {
+	
+	public static void main(String[] args) {
+		ClaseB obj = new ClaseB();
+		obj.metodoB();
+	}
+
+}
---
+++ Semana02/Semana02/src/pe/edu/uni/semana02/uno/ClaseB.java
@@ -0,0 +1,17 @@
+package pe.edu.uni.semana02.uno;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class ClaseB {
+	
+	public void metodoB(){
+		ClaseA obj = new ClaseA();
+		//System.out.println("n1: " + obj.n1);
+		System.out.println("n2: " + obj.n2);
+		System.out.println("n3: " + obj.n3);
+		System.out.println("n4: " + obj.n4);
+	}
+
+}
